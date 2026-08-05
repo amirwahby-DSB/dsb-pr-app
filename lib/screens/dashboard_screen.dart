b1bf7f3f940282c4f0a78d4d823664921abe8577
amirwahby-DSB <amir.wahby@gmail.com>
2026-08-05 11:18:35 +0300

    Add service catalog strings

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -229,4 +229,20 @@ class _RequestMiniCard extends StatelessWidget {
       ),
     );
   }
-}
+static String get serviceGuideTitle => switch (_lang) {
+        AppLanguage.ar => 'دليل الخدمات – الأقسام الستة',
+        AppLanguage.en => 'Service Guide – Six Pillars',
+        AppLanguage.de => 'Leistungsverzeichnis – Sechs Säulen',
+      };
+
+  static String get searchServiceHint => switch (_lang) {
+        AppLanguage.ar => 'ابحث عن خدمة (تأشيرة، طباعة، رحلة...)',
+        AppLanguage.en => 'Search a service (visa, printing, trip...)',
+        AppLanguage.de => 'Dienst suchen (Visum, Druck, Reise...)',
+      };
+
+  static String get requestButton => switch (_lang) {
+        AppLanguage.ar => 'طلب',
+        AppLanguage.en => 'Request',
+        AppLanguage.de => 'Anfrage',
+      };}
