ec22481503c23e8793a9db6fa491de80f8dc03dd
amirwahby-DSB <amir.wahby@gmail.com>
2026-08-08 11:06:49 +0300

    Fix dashboard_screen build error - move strings to separate file

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dashboard_strings.dart';
 import '../models/pr_request.dart';
 import '../models/app_user.dart';
 import '../services/mock_data_service.dart';
@@ -229,24 +230,3 @@ class _RequestMiniCard extends StatelessWidget {
       ),
     );
   }
- class _Strings {
-  static AppLanguage get _lang => LocaleService.instance.language;
-
-  static String get serviceGuideTitle => switch (_lang) {
-        AppLanguage.ar => 'دليل الخدمات - الأقسام الستة',
-        AppLanguage.en => 'Service Guide - Six Pillars',
-        AppLanguage.de => 'Leistungsverzeichnis - Sechs Säulen',
-      };
-
-  static String get searchServiceHint => switch (_lang) {
-        AppLanguage.ar => 'ابحث عن خدمة (تأشيرة، طباعة، رحلة...)',
-        AppLanguage.en => 'Search a service (visa, printing, trip...)',
-        AppLanguage.de => 'Dienst suchen (Visum, Druck, Reise...)',
-      };
-
-  static String get requestButton => switch (_lang) {
-        AppLanguage.ar => 'طلب',
-        AppLanguage.en => 'Request',
-        AppLanguage.de => 'Antrag',
-      };
-}
